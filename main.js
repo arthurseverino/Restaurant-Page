84a1f034278b08fab28bd23294ce4a4dba50a42b
Arthur Severino <aas1181@nyu.edu>
2023-10-17 16:58:51 -0400

    set up project

---
+++ main.js
@@ -0,0 +1,26 @@
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+/*
+
+imports go here 
+3 of them 
+
+*/
+
+alert('hello2');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBOztBQUVBO0FBQ0E7O0FBRUE7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLypcblxuaW1wb3J0cyBnbyBoZXJlIFxuMyBvZiB0aGVtIFxuXG4qL1xuXG5hbGVydCgnaGVsbG8yJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
